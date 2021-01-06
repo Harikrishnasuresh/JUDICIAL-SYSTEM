15aa8748312f6ce145ca5d47ad462bb9ec8ab0df
Harikrishnasuresh <56651532+Harikrishnasuresh@users.noreply.github.com>
2021-01-06 20:29:03 +0530

    Add files via upload

---
+++ version 4/JUDICIAL MANAGEMENT PROJECT/caseadmin.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace JUDICIAL_MANAGEMENT_PROJECT
+{
+    partial class caseadmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(caseadmin));
+            this.SuspendLayout();
+            // 
+            // caseadmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1181, 852);
+            this.MaximumSize = new System.Drawing.Size(1202, 901);
+            this.MinimumSize = new System.Drawing.Size(1199, 899);
+            this.Name = "caseadmin";
+            this.Text = "caseadmin";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ version 4/JUDICIAL MANAGEMENT PROJECT/caseadmin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JUDICIAL_MANAGEMENT_PROJECT
+{
+    public partial class caseadmin : Form
+    {
+        public caseadmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
